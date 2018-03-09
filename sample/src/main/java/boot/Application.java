24a0304317e3c32adfae08c0e36b46b25c2c8189
lijuntaopro <lijuntao_pro@163.com>
2018-03-09 15:33:40 +0800

    1、试验boot打包

--- sample/src/main/java/boot/Application.java
+++ sample/src/main/java/boot/Application.java
@@ -3,12 +3,17 @@ package boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Configuration
-@EnableWebMvc
+@EnableAutoConfiguration
 public class Application {
-
+	
+	@RequestMapping("/**")
+	public String hello(){
+		return "Hello World";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
